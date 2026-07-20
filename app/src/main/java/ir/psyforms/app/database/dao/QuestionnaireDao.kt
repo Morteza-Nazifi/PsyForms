031386b7e0d36afb1a5a98156382841c81aa432d
Morteza-Nazifi <mnazifee@gmail.com>
2026-07-20 07:21:15 +0330

    Create QuestionnaireDao.kt

---
+++ app/src/main/java/ir/psyforms/app/database/dao/QuestionnaireDao.kt
@@ -0,0 +1,41 @@
+package ir.psyforms.app.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import androidx.room.Query
+import ir.psyforms.app.database.entity.QuestionnaireEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface QuestionnaireDao {
+
+    @Insert
+    suspend fun insert(questionnaire: QuestionnaireEntity): Long
+
+    @Update
+    suspend fun update(questionnaire: QuestionnaireEntity)
+
+    @Delete
+    suspend fun delete(questionnaire: QuestionnaireEntity)
+
+    @Query("""
+        SELECT * FROM questionnaires
+        ORDER BY displayOrder
+    """)
+    fun getAll(): Flow<List<QuestionnaireEntity>>
+
+    @Query("""
+        SELECT * FROM questionnaires
+        WHERE domainId = :domainId
+        ORDER BY displayOrder
+    """)
+    fun getByDomain(domainId: Long): Flow<List<QuestionnaireEntity>>
+
+    @Query("""
+        SELECT * FROM questionnaires
+        WHERE id = :id
+    """)
+    suspend fun getById(id: Long): QuestionnaireEntity?
+}
